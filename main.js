5e5f258c1ed8ec88e8fd98cffdac3d36497fd783
Jackson Ming Hu <huming2207@gmail.com>
2018-04-23 12:41:31 +1000

    Assignment 2: initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+"use strict";
+  
+// returns square bipyramid (octahedron) object
+function createSquareBipyramid(sizeX, sizeY, sizeZ, material)
+{
+
+}
+
+// returns pentagonal bipyramid (decahedron) object
+function createPentagonalBipyramid(sizeX, sizeY, sizeZ, material)
+{
+
+}
+
+// returns joint axes object
+function createJoint(name)
+{
+
+}
+
+// returns a whole leg
+function createLeg(end, side, material)
+{
+
+}
+
+// returns the torso object
+function createTorso(material)
+{
+
+}
+
+// returns the head object
+function createHead(material)
+{
+
+}
+
+// Uses the other functions to create the frog
+function createFrog(material)
+{
+
+}
